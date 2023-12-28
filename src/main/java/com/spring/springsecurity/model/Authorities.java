57b5050ae8ff97f9aa839f258f8b30c19921d037
GADO89 <gadomahmoud377@gmail.com>
2023-12-28 18:30:50 +0200

    # create model User - Role - Authorities

---
+++ src/main/java/com/spring/springsecurity/model/Authorities.java
@@ -0,0 +1,16 @@
+package com.spring.springsecurity.model;
+
+import jdk.javadoc.internal.doclets.toolkit.taglets.SeeTaglet;
+
+import java.util.Set;
+
+public class Authorities {
+    private Long id;
+
+    private String authoritiesName;
+
+    private Set<User> users;
+
+
+
+}
