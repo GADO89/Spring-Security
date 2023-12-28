5cc38225e1ca52eb618ba86d9e892aeabd854e4d
GADO89 <gadomahmoud377@gmail.com>
2023-12-28 22:52:16 +0200

    # CommandLineRunner with DB

--- src/main/java/com/spring/springsecurity/model/Authorities.java
+++ src/main/java/com/spring/springsecurity/model/Authorities.java
@@ -2,6 +2,7 @@ package com.spring.springsecurity.model;
 
 
 import javax.persistence.*;
+import java.util.List;
 import java.util.Set;
 
 @Entity
@@ -16,8 +17,36 @@ public class Authorities {
     private String authoritiesName;
 
     @ManyToMany(mappedBy = "authorities")
-    private Set<User> users;
+    private List<User> users;
 
+    public Authorities(String authoritiesName) {
+        this.authoritiesName = authoritiesName;
+    }
 
+    public Authorities() {
+    }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAuthoritiesName() {
+        return authoritiesName;
+    }
+
+    public void setAuthoritiesName(String authoritiesName) {
+        this.authoritiesName = authoritiesName;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
 }
