d109c0a6070d337be439fc9e8e2bce99c7955b51
GADO89 <gadomahmoud377@gmail.com>
2023-12-28 20:56:43 +0200

    hibernate and jpa with models

--- src/main/java/com/spring/springsecurity/model/Authorities.java
+++ src/main/java/com/spring/springsecurity/model/Authorities.java
@@ -1,14 +1,21 @@
 package com.spring.springsecurity.model;
 
-import jdk.javadoc.internal.doclets.toolkit.taglets.SeeTaglet;
 
+import javax.persistence.*;
 import java.util.Set;
 
+@Entity
+@Table(name = "authorities")
 public class Authorities {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "authorities_name")
     private String authoritiesName;
 
+    @ManyToMany(mappedBy = "authorities")
     private Set<User> users;
 
 
