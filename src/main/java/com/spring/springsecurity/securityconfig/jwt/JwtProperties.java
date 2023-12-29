9d45ce8b95e18b0233248c70d63f1608b154ee55
GADO89 <gadomahmoud377@gmail.com>
2023-12-29 20:14:51 +0200

    create JwtLogin , JwtProperties and add jwt dependency

---
+++ src/main/java/com/spring/springsecurity/securityconfig/jwt/JwtProperties.java
@@ -0,0 +1,10 @@
+package com.spring.springsecurity.securityconfig.jwt;
+
+public class JwtProperties {
+
+    public static final String secret="MYSECRET";
+    public static final int EXPIRATION_TIME=864_000_000;   //10 DAYS
+    public static final String TOKEN_PREFIX="Bearer";
+    public static final String HEADER_STRING="Authorization";
+
+}
