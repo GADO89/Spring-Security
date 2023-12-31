b13f96f1a7f750f0494065f6298c5cde43e36fe3
GADO89 <gadomahmoud377@gmail.com>
2023-12-31 17:33:52 +0200

    editing SecurityConfiguration

--- src/main/java/com/spring/springsecurity/securityconfig/SecurityConfiguration.java
+++ src/main/java/com/spring/springsecurity/securityconfig/SecurityConfiguration.java
@@ -1,30 +1,37 @@
 package com.spring.springsecurity.securityconfig;
 
+import com.spring.springsecurity.dao.UserRepository;
+import com.spring.springsecurity.securityconfig.jwt.JwtAuthenticationFilter;
+import com.spring.springsecurity.securityconfig.jwt.JwtAuthorizationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.authentication.configurers.userdetails.DaoAuthenticationConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
-public class securityConfiguration extends WebSecurityConfigurerAdapter {
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     private UserPrincipleDetailsService userPrincipleDetailsService;
 
-    @Autowired
-    public securityConfiguration( UserPrincipleDetailsService userPrincipleDetailsService) {
-        this.userPrincipleDetailsService = userPrincipleDetailsService;
-    }
+    private UserRepository userRepository;
 
     @Autowired
+    public SecurityConfiguration(UserPrincipleDetailsService userPrincipleDetailsService, UserRepository userRepository) {
+        this.userPrincipleDetailsService = userPrincipleDetailsService;
+        this.userRepository = userRepository;
+    }
+
+
+
 
 
     @Override
@@ -38,8 +45,13 @@ public class securityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
     http
+            .csrf().disable()
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
+            .addFilter(new JwtAuthenticationFilter(authenticationManager()))
+            .addFilter(new JwtAuthorizationFilter(authenticationManager(),this.userRepository ))
             .authorizeRequests()
-
+            .antMatchers(HttpMethod.POST,"/login").permitAll()
             .antMatchers("/api/main").permitAll()
             .antMatchers("/api/profile").authenticated()
             .antMatchers("/api/admin/**").hasRole("ADMIN")
@@ -47,19 +59,7 @@ public class securityConfiguration extends WebSecurityConfigurerAdapter {
             .antMatchers("/api/basic/mybasic").hasAuthority("ACCESS_BASIC1")
             .antMatchers("/api/basic/allbasic").hasAuthority("ACCESS_BASIC2")
 
-
-
-            //.anyRequest().authenticated()
-            .and()
-            .formLogin()
-            .loginProcessingUrl("/signin")
-            .loginPage("/api/login")
-            .usernameParameter("user")
-            .passwordParameter("pass")
-            .and()
-            .logout()
-            .logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/api/login");
-
+            .anyRequest().authenticated();
 
     }
     @Bean
